b4aca8f685218191d2e5d7209698500c9fd79bdb
Robert Sturla <robertsturla@outlook.com>
2024-08-04 12:17:38 +0100

    feature: support fetching configuration from SSM

--- services/ingest-service/pkg/config/config.go
+++ services/ingest-service/pkg/config/config.go
@@ -1,9 +1,19 @@
 package config
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"reflect"
+	"sync"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+)
+
+var (
+	ssmClient     *ssm.Client
+	ssmClientOnce sync.Once
 )
 
 // NewConfig retrieves all required environment variables and returns a Config struct.
@@ -15,13 +25,22 @@ func NewConfig[T any]() (*T, error) {
 	for i := 0; i < v.NumField(); i++ {
 		field := v.Field(i)
 		envVarName := typeOfConfig.Field(i).Tag.Get("env")
-		if envVarName == "" {
-			envVarName = typeOfConfig.Field(i).Name
+		ssmVarName := typeOfConfig.Field(i).Tag.Get("ssm")
+
+		var value string
+		var err error
+		switch {
+		case ssmVarName != "":
+			value, err = getSSMParameter(ssmVarName)
+		case envVarName != "":
+			value, err = getEnvironmentVariable(envVarName)
+		default:
+			continue
 		}
-		value, err := getEnvironmentVariable(envVarName)
 		if err != nil {
 			return nil, err
 		}
+
 		field.SetString(value)
 	}
 
@@ -35,4 +54,23 @@ func getEnvironmentVariable(key string) (string, error) {
 		return "", fmt.Errorf("missing required environment variable: %s", key)
 	}
 	return value, nil
+}
+
+// getSSMParameter fetches the value of an SSM parameter, decrypting it if necessary.
+func getSSMParameter(key string) (string, error) {
+	// Initialize the SSM client only once
+	ssmClientOnce.Do(func() {
+		ssmClient = ssm.NewFromConfig(aws.Config{})
+	})
+
+	param, err := ssmClient.GetParameter(context.TODO(), &ssm.GetParameterInput{
+		Name:           aws.String(key),
+		WithDecryption: aws.Bool(true),
+	})
+
+	if err != nil {
+		return "", fmt.Errorf("failed to get parameter %s from SSM: %v", key, err)
+	}
+
+	return *param.Parameter.Value, nil
 }
